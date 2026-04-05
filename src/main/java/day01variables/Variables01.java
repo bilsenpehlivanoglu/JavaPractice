57cb11ea55a04173db5d5a734e46be30dbc04f99
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-04-05 15:58:20 +0200

    v

---
+++ src/main/java/day01variables/Variables01.java
@@ -0,0 +1,19 @@
+package day01variables;
+
+public class Variables01 {
+
+    public static void main(String[] args) {
+
+        //Java bu satiri okumaz, it is a comment line.
+
+        /*
+        It is also comment line.
+         */
+
+        //Variable is a passive features
+        //How can I create a variable?
+        //Data type + varieble name + Assigment operator + variable value + noktali virgul
+
+        int age = 13;
+    }
+}
