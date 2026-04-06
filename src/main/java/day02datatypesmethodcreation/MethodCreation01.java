ff2e19d3a773e27646db97f225653778a71c61c8
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-04-06 19:42:02 +0200

    v

--- src/main/java/day02datatypesmethodcreation/MethodCreation01.java
+++ src/main/java/day02datatypesmethodcreation/MethodCreation01.java
@@ -1,7 +1,17 @@
 package day02datatypesmethodcreation;
 
 public class MethodCreation01 {
+
     public static void main(String[] args) {
 
+
     }
+
+    //Create a method to perform the addition operation.
+    public int addition(int a, int b){
+        return a+b; //return demek:methodun cagrildigi yere return eder.
+    }
+
+
+
 }
