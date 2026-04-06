26e47409d7c15ba2f48d279fa765a3a46f8362a9
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-04-06 19:51:20 +0200

    v

--- src/main/java/day02datatypesmethodcreation/MethodCreation01.java
+++ src/main/java/day02datatypesmethodcreation/MethodCreation01.java
@@ -4,15 +4,16 @@ public class MethodCreation01 {
 
     public static void main(String[] args) {
 
-
+    int solution = addition(3,5);
+        System.out.println(solution);
 
     }
     //method olusturmak icin: access modifier + return type + method ismi + () + {}
     //Create a method to perform the addition operation.
-    public int addition(int a, int b){
+    public static int addition(int a, int b){
         return a+b; //return demek:methodun cagrildigi yere return eder.
     }
-
+   //main method static oldugu icin, main method icinde kullanacagimiz hersey static olmalidir.
 
 
 }
