c0f2d6f203aad904c46eeaea5183c14fd05f3570
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-04-06 17:05:14 +0200

    v

---
+++ src/main/java/day02datatypesmethodcreation/Variables02.java
@@ -0,0 +1,10 @@
+package day02datatypesmethodcreation;
+
+public class Variables02 {
+    public static void main(String[] args) {
+
+        //Create a variable for the city name and print it to the screen.
+        String cityName = "Canada";
+        System.out.println(cityName);
+    }
+}
