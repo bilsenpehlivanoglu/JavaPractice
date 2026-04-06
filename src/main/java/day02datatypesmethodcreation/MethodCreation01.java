ae108cc5badd5286e30f859aba91c0677ce451f1
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-04-06 19:43:47 +0200

    v

--- src/main/java/day02datatypesmethodcreation/MethodCreation01.java
+++ src/main/java/day02datatypesmethodcreation/MethodCreation01.java
@@ -5,8 +5,9 @@ public class MethodCreation01 {
     public static void main(String[] args) {
 
 
-    }
 
+    }
+    //method olusturmak icin: access modifier + return type + method ismi + () + {}
     //Create a method to perform the addition operation.
     public int addition(int a, int b){
         return a+b; //return demek:methodun cagrildigi yere return eder.
