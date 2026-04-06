3585b05001b33b33b047ce06d86e69b1b88f60b5
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-04-06 16:20:22 +0200

    v

--- src/main/java/day02datatypesmethodcreation/Variables01.java
+++ src/main/java/day02datatypesmethodcreation/Variables01.java
@@ -1,4 +1,12 @@
 package day02datatypesmethodcreation;
 
 public class Variables01 {
+
+    public static void main(String[] args) {
+
+   //Create a variable for the city name, assign it the value "Boston" then "New Hampshire," and print them to the screen.
+
+        String cityName = "Boston";
+        String cityName2 ="New Hampshire";
+    }
 }
