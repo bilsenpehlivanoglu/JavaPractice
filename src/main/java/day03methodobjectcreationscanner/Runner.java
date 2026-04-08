12895e3b1a927559ca8e32e9e07c3f9727ddfc18
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-04-08 22:17:47 +0200

    v

--- src/main/java/day03methodobjectcreationscanner/Runner.java
+++ src/main/java/day03methodobjectcreationscanner/Runner.java
@@ -4,7 +4,8 @@ public class Runner {
 
     public static void main(String[] args) {
         //object nasil olusturulur?
-
+        // class ismi + object ismi + assigment operator + "new" keyword + constructor
+          Car myCar = new Car();//
 
     }
 
