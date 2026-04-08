1d59ad62b7865ccef876eabef88d7dfb76604fff
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-04-08 22:34:07 +0200

    v

--- src/main/java/day03methodobjectcreationscanner/Runner.java
+++ src/main/java/day03methodobjectcreationscanner/Runner.java
@@ -4,8 +4,13 @@ public class Runner {
 
     public static void main(String[] args) {
         //object nasil olusturulur?
+        //Constructor:Java da objectleri olusturmak icin kullanilan bir method dur.
         // class ismi + object ismi + assigment operator + "new" keyword + constructor
           Car myCar = new Car();//
+
+        System.out.println("myCar.fiyat = " + myCar.fiyat);
+        System.out.println("myCar.model = " + myCar.model);
+
 
     }
 
