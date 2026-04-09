f3144cee8da03a5691846e6fb22b1bd40bbe4658
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-04-09 20:00:22 +0200

    v

--- src/main/java/day04scannerwrapper/Scanner02.java
+++ src/main/java/day04scannerwrapper/Scanner02.java
@@ -1,4 +1,17 @@
 package day04scannerwrapper;
 
+import java.util.Scanner;
+
 public class Scanner02 {
+    public static void main(String[] args) {
+
+        //Kullanicidan adresini alniz ve ekrana yazdiriniz
+        //Get user's address and print it on the screen
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your address");
+        String address = input.nextLine();//nextLine uzun kelimeleri alir
+
+        System.out.println(address);
+
+    }
 }
