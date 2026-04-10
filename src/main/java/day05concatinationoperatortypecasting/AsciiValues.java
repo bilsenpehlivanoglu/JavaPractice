fbca099819be7813721da2937430d01a9799d135
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-04-10 21:31:04 +0200

    v

--- src/main/java/day05concatinationoperatortypecasting/AsciiValues.java
+++ src/main/java/day05concatinationoperatortypecasting/AsciiValues.java
@@ -4,6 +4,13 @@ public class AsciiValues {
     public static void main(String[] args) {
 
         char ch = 'A';
-        System.out.println(ch);
+        System.out.println(ch);//A
+
+        //ascii degeri gormek icin
+        int k = 'A';
+        System.out.println(k);//65
+
+        int unlem = '!';
+        System.out.println(unlem);//33
     }
 }
