5e2eb3534d13f92e10d4ad9e7d39df763d59e68c
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-04-10 22:29:38 +0200

    v

--- src/main/java/day05concatinationoperatortypecasting/TypeCasting.java
+++ src/main/java/day05concatinationoperatortypecasting/TypeCasting.java
@@ -1,4 +1,26 @@
 package day05concatinationoperatortypecasting;
 
 public class TypeCasting {
+    public static void main(String[] args) {
+
+        //byte data type int data type a ceviriniz
+        //Convert byte data type to int data type
+        byte age = 13;
+        int ageInt = age;//autowidening
+
+        //int data type ini short data type ina cevirin
+        //convert int data type to short data type
+        int weight = 313;
+        short weightShort = (short) weight;//explicitnarrowing
+
+        //int data type i float data type a cevirin
+        //convert int data type to float data type.
+        int popupation = 123;
+        System.out.println(popupation);
+        float populationFloat = popupation;//Auto widening
+        System.out.println(popupation);
+
+
+    }
+
 }
