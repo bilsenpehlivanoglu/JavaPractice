d7ba562edb798a0e794dedb9d2fdf2e4bf042206
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-04-10 16:34:17 +0200

    v

---
+++ src/main/java/day05concatinationoperatortypecasting/Concatination.java
@@ -0,0 +1,14 @@
+package day05concatinationoperatortypecasting;
+
+public class Concatination {
+    public static void main(String[] args) {
+
+        //Bir string ve iki int variable olusturun.String degeri ile int lerin toplamini yazdirin.
+        //Create a string and two integer variables. Print the sum of the string value and the integer values.
+        String s = "elma";
+        int a = 10;
+        int b = 11;
+        System.out.println(s+a+b);//elma1011
+
+    }
+}
