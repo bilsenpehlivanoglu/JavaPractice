a1a7a675be340a3e8e0954ad642401f33a6f930e
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-04-10 21:27:45 +0200

    v

---
+++ src/main/java/day05concatinationoperatortypecasting/AsciiValues.java
@@ -0,0 +1,9 @@
+package day05concatinationoperatortypecasting;
+
+public class AsciiValues {
+    public static void main(String[] args) {
+
+        char ch = 'A';
+        System.out.println(ch);
+    }
+}
--- src/main/java/day05concatinationoperatortypecasting/Operators.java
+++ src/main/java/day05concatinationoperatortypecasting/Operators.java
@@ -6,6 +6,14 @@ public class Operators {
 
         boolean first = 3<5;//true
         boolean second = 2+3!=5;//false
+        boolean third = 2+3*5>=19;//false
+        System.out.println(third);
         System.out.println(first + "-"+second);
+
+        System.out.println(first&&second);//false
+        System.out.println(first || second || third);//true
+
+
+
     }
 }
