cbfd67330b9568734b3cd7baa21efcef45194859
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-04-10 18:24:40 +0200

    v

--- src/main/java/day05concatinationoperatortypecasting/Operators.java
+++ src/main/java/day05concatinationoperatortypecasting/Operators.java
@@ -4,5 +4,8 @@ public class Operators {
     public static void main(String[] args) {
 
 
+        boolean first = 3<5;//true
+        boolean second = 2+3!=5;//false
+        System.out.println(first + "-"+second);
     }
 }
