ef5eca716563fac0c3997e61d108ac560b361de9
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-04-11 14:19:55 +0200

    v

--- src/main/java/day06swapvaluesstringmanipulations/StringManipulations01.java
+++ src/main/java/day06swapvaluesstringmanipulations/StringManipulations01.java
@@ -12,5 +12,10 @@ public class StringManipulations01 {
         //Make all characters uppercase.
         String sUpper = s.toUpperCase();
         System.out.println(sUpper);//JAVA IS EASY
+
+        //example 2:S stringdeki tum characterleri kucuk harf yapiniz.
+        //Convert all the characters to lowercase in the sitring.
+        String sLower = s.toLowerCase();
+        System.out.println(sLower);//java is easy
     }
 }
