61af1447ec300ce6a5aabcdeee7786af406d7a9d
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-04-11 14:08:43 +0200

    vv

--- src/main/java/day06swapvaluesstringmanipulations/StringManipulations01.java
+++ src/main/java/day06swapvaluesstringmanipulations/StringManipulations01.java
@@ -1,4 +1,12 @@
 package day06swapvaluesstringmanipulations;
 
 public class StringManipulations01 {
+    public static void main(String[] args) {
+
+      //String bir non-primitive data type dir ve ayni zamanda bir class dir.
+     //String is a non-primitive data type and also a class.
+
+        String s ="Java is easy";
+
+    }
 }
