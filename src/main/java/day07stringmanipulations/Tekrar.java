f0d296f9d5a6a76be94b0352af415e159daa2fe7
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-04-12 23:34:08 +0200

    v

--- src/main/java/day07stringmanipulations/Tekrar.java
+++ src/main/java/day07stringmanipulations/Tekrar.java
@@ -10,5 +10,26 @@ public class Tekrar {
         System.out.println(s);
         String s1 = s.trim();
         System.out.println(s1);
+
+        //example 2:urunlerin fiyatlarini toplayiniz.
+        //Sum the prices of the products.
+        String tv = "$125.99";
+        String laptop = "$250.99";
+
+        String laptop1 = laptop.replace("$","");
+        String tv1 = tv.replace("$","");
+
+        Double sum = Double.valueOf(tv1)+Double.valueOf(laptop1);
+        System.out.println(sum);
+
+        //example 3:verilen ismin ilk harfini ve soy isminin ilk harfini ekrana yazdirin
+        //Print the first letter of the given first name and the first letter of the given last name to the screen.
+        //  Ali Can  ==>AC
+        String s2 = "  Ali Can  ";
+        char first = s2.trim().toUpperCase().charAt(0);
+
+        char last = s2.trim().toUpperCase().split(" ")[1].charAt(0);
+
+        System.out.println("" +first + last);
     }
 }
