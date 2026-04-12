2faeee30af864db406a5e8184fadcc9923eb0ea0
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-04-12 20:47:18 +0200

    v

--- src/main/java/day07stringmanipulations/StringManipulations03.java
+++ src/main/java/day07stringmanipulations/StringManipulations03.java
@@ -12,6 +12,18 @@ public class StringManipulations03 {
         String sTrimmed = s.trim();
         System.out.println("sTrimmed = " + sTrimmed);
 
+        //example 2:urunlerin fiyatlarini toplayiniz.
+        //Sum the prices of the products.
+        String tv = "$456.99";
+        String laptop = "$875.99";
+
+        String tv2 = tv.replace("$","");
+        System.out.println("tv2 = " + tv2);
+        String laptop2 = laptop.replace("$","");
+        System.out.println(laptop2);
+
+        Double sum =Double.valueOf(tv2) + Double.valueOf(laptop2);
+        System.out.println(sum);
 
     }
 }
