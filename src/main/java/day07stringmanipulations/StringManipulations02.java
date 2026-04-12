c5d0b11355a1017d8e21fc9ae7f765efa3ab7d14
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-04-12 16:47:19 +0200

    v

---
+++ src/main/java/day07stringmanipulations/StringManipulations02.java
@@ -0,0 +1,36 @@
+package day07stringmanipulations;
+
+import java.util.Scanner;
+
+public class StringManipulations02 {
+    public static void main(String[] args) {
+
+        /*
+        example:
+        asagidaki kurallara gore kullanicinin girdigi password u kontrol ediniz.
+        1)en az 8 karakter olsun/It must be at least 8 characters long.
+        2)space karakteri passworda olmasin//The password must not contain spaces.
+        3)en az bir buyuk harf olsun//at least one uppercase letter
+        4)en az bir kucuk harf olsun//at least one lowercase letter
+        5)en az bir rakam olsun//at least one number
+
+         */
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your password");
+        String pwd = input.nextLine();
+
+        // 1)en az 8 karakter olsun
+        boolean first = pwd.length()>7;
+        System.out.println("first = "+first);
+
+        //2)space karakteri passworda olmasin
+        boolean second = !pwd.contains(" ");
+        System.out.println("second = " + second);
+
+        //3)en az bir buyuk harf olsun
+        boolean third = pwd.replaceAll("[^A-Z]","").length()>0;
+        System.out.println("third = " + third);
+
+
+    }
+}
