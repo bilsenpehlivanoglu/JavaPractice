e8b3297c43d746019606d189558682b29eec6ed0
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-04-12 22:15:32 +0200

    v

---
+++ src/main/java/day07stringmanipulations/Tekrar.java
@@ -0,0 +1,10 @@
+package day07stringmanipulations;
+
+public class Tekrar {
+    public static void main(String[] args) {
+
+        //example 1:bir stringin bas ve sonunda space karakteri varsa siliniz.
+        //If a string contains a space at the beginning or end, delete them.
+        //" Ali Can " ==>Ali Can
+    }
+}
