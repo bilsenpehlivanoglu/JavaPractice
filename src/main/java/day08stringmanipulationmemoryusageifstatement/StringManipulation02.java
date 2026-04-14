b503a9963a2318b148d7ec307f23d1f975665d03
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-04-14 16:22:24 +0200

    v

--- src/main/java/day08stringmanipulationmemoryusageifstatement/StringManipulation02.java
+++ src/main/java/day08stringmanipulationmemoryusageifstatement/StringManipulation02.java
@@ -6,9 +6,16 @@ public class StringManipulation02 {
         //example 1:String in belirli bir karakterinden baslayip belirli bir karaktere kadar dinamic bir sekilde aliniz.
         //Write a string dynamically, starting from a specific character and ending up with another specific character.
         String a = "abc@gmail.com";
-        int starting = a.indexOf('@');
+        int starting = a.indexOf('@')+1;
         int ending = a.indexOf('.');
         String company = a.substring(starting,ending);
-        System.out.println(company);
+        System.out.println(company);//gmail
+
+        //example 2:verilen 2 stringin birbirinin aynisi olup olmadigini kontrol edin.
+        //Check if the two strings are the same.
+        String h = "ali can";
+        String i = "Ali Can";
+        boolean result = h.equals(i);
+        System.out.println(result);//false
     }
 }
