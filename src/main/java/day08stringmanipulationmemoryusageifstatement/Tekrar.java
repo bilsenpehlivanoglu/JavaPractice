6dae02582d8fd825b54e47f6607a47aafe5c43da
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-04-15 16:35:38 +0200

    v

--- src/main/java/day08stringmanipulationmemoryusageifstatement/Tekrar.java
+++ src/main/java/day08stringmanipulationmemoryusageifstatement/Tekrar.java
@@ -13,6 +13,16 @@ public class Tekrar {
         boolean result1 = s1.isBlank();
         System.out.println(result1);//true
 
+        //Print the sum of the indexes of the characters a, i, and e in a string.
+        //Java is esay to learn ==>
+        String s2 = "Java is esay to learn";
+        int ida = s2.indexOf('a');
+        int idi = s2.indexOf('i');
+        int ide = s2.indexOf('e');
+        System.out.println(ide + ida + idi);//14
+
+        //Print the sum of the indexes of the characters j, y, and o in a string.
+        //Java is esay to learn ==>
 
     }
 }
