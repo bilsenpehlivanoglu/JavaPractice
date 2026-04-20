0593a938f08088b0a2763e3ba1ce90dc91fde921
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-04-20 18:31:46 +0200

    v

--- src/main/java/day08stringmanipulationmemoryusageifstatement/IfStatements.java
+++ src/main/java/day08stringmanipulationmemoryusageifstatement/IfStatements.java
@@ -3,6 +3,17 @@ package day08stringmanipulationmemoryusageifstatement;
 public class IfStatements {
     public static void main(String[] args) {
 
+//if you study hard, you will learn Java
+
+        /*
+        if(you study harf){
+        you will learn Java ==? Java
+        }
+         */
+
+        //example 1:sayi pozitif ise ekrana pozitif yazdirin.
+        //If the number is positive, print "positive" to the screen.
+
 
     }
 }
