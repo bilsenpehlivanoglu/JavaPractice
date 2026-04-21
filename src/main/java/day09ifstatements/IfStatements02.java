dc1a154f284468f36631c702717dd331295b8b3c
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-04-21 23:25:53 +0200

    v

--- src/main/java/day09ifstatements/IfStatements02.java
+++ src/main/java/day09ifstatements/IfStatements02.java
@@ -1,7 +1,20 @@
 package day09ifstatements;
 
+import java.util.Scanner;
+
 public class IfStatements02 {
     public static void main(String[] args) {
+
+        //Kullanicidan alinan sayinin tek mi cift mi oldugunu yaziniz.
+        //Print whether the number received from the user is odd or even.
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int num = input.nextInt();
+
+        if (num%2 == 0){
+            System.out.println("The number is even");
+        }
 
     }
 }
