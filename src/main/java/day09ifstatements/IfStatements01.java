f393f8382a98731551cbe99ce93c0e3a47a5be36
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-04-21 14:07:38 +0200

    v

--- src/main/java/day09ifstatements/IfStatements01.java
+++ src/main/java/day09ifstatements/IfStatements01.java
@@ -3,5 +3,11 @@ package day09ifstatements;
 public class IfStatements01 {
     public static void main(String[] args) {
 
+        //example 1:Sayi uc basamakli ise ekrana "sayi uc basamaklidir" yazdirin.
+        //If the number is three digits , print "The number is three digits" to the screen.
+        int number = 123;
+        if (number>99 || number<1000){
+            System.out.println("The number is three digits");
+        }
     }
 }
