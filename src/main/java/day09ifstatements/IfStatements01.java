50156d0a9b1b37571ce1f617bafb0997409a851b
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-04-21 14:51:23 +0200

    v

--- src/main/java/day09ifstatements/IfStatements01.java
+++ src/main/java/day09ifstatements/IfStatements01.java
@@ -10,5 +10,28 @@ public class IfStatements01 {
         if (number>99 && number<1000){
             System.out.println("The number is three digits");
         }
+
+        //example 2:Bir stringdeki tekrarsiz karakterleri ekrana yazdiriniz.
+        //Print only the non-repeating characters in a string.
+        // abbccdc ==>ad
+        String str = "aac";
+
+        char ch1 = str.charAt(0);
+
+        if (str.indexOf(ch1) == str.lastIndexOf(ch1)){
+            System.out.println(ch1);
+        }
+
+        char ch2 = str.charAt(1);
+
+        if (str.indexOf(ch2) == str.lastIndexOf(ch2)){
+            System.out.println(ch2);
+        }
+
+        char ch3 = str.charAt(2);
+
+        if (str.indexOf(ch3) == str.lastIndexOf(ch3)){
+            System.out.println(ch3);
+        }
     }
 }
