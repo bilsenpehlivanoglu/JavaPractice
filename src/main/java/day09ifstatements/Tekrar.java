5f25497ceed7040d9609e42e4ff94159198af73b
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-04-22 22:08:30 +0200

    v

---
+++ src/main/java/day09ifstatements/Tekrar.java
@@ -0,0 +1,13 @@
+package day09ifstatements;
+
+public class Tekrar {
+    public static void main(String[] args) {
+
+        //If the number is positive, print "positive" to the screen.
+        int number = 14;
+
+        if (number>-1){
+            System.out.println("The number is positive");
+        }
+    }
+}
