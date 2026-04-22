02ce64041f5d436ccbfbf957128bff70c948cdac
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-04-22 22:10:27 +0200

    v

--- src/main/java/day09ifstatements/Tekrar.java
+++ src/main/java/day09ifstatements/Tekrar.java
@@ -4,10 +4,17 @@ public class Tekrar {
     public static void main(String[] args) {
 
         //If the number is positive, print "positive" to the screen.
-        int number = 14;
+        int number = 0;
 
         if (number>-1){
             System.out.println("The number is positive");
+        }
+
+        //If the number is between -1 and 10, print "number" to the screen.
+        int number1 = 9;
+
+        if (number1>-1 && number1<10){
+            System.out.println("Number");
         }
     }
 }
