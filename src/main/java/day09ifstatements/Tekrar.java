b01171211802896ee3961893e7c587eae8552a21
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-04-22 22:21:41 +0200

    v

--- src/main/java/day09ifstatements/Tekrar.java
+++ src/main/java/day09ifstatements/Tekrar.java
@@ -4,17 +4,25 @@ public class Tekrar {
     public static void main(String[] args) {
 
         //If the number is positive, print "positive" to the screen.
-        int number = 0;
+       /* int number = 0;
 
         if (number>-1){
             System.out.println("The number is positive");
-        }
+        }*/
 
         //If the number is between -1 and 10, print "number" to the screen.
-        int number1 = 9;
+        /*int number1 = -1;
 
         if (number1>-1 && number1<10){
             System.out.println("Number");
+        }*/
+
+        //If the number is three digits , print "The number is three digits" to the screen.
+        int number2 = 123;
+
+        if (number2>99 && number2<1000){
+            System.out.println("The number is three digits");
         }
+
     }
 }
