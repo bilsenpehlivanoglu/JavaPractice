7f4cc3fed51414f7164f2765c160673965ec53a5
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-04-23 18:51:57 +0200

    v

---
+++ src/main/java/day11nestedifternaryincrementdecrement/Ternary01.java
@@ -0,0 +1,18 @@
+package day11nestedifternaryincrementdecrement;
+
+public class Ternary01 {
+    public static void main(String[] args) {
+
+        //Ternary Statement
+        //example 1:sayi 10 dan kucuk ise kucuk yazdirin, degilse kucuk degil yazdirin.
+
+        //if else cozumu
+        int num = 3;
+
+        if (num<10){
+            System.out.println("small");
+        }else {
+            System.out.println("not small");
+        }
+    }
+}
