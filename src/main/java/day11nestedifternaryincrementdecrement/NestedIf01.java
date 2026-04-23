83d90e63cebf71930f7f0b3d74174c19b407f591
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-04-23 17:56:40 +0200

    v

---
+++ src/main/java/day11nestedifternaryincrementdecrement/NestedIf01.java
@@ -0,0 +1,18 @@
+package day11nestedifternaryincrementdecrement;
+
+public class NestedIf01 {
+    public static void main(String[] args) {
+
+        /*
+        Kullanicidan 0'dan kucuk 120 den buyuk deger giremiyecek sekilde datalari aldiktan sonra
+
+        Eger calisan kadin ise;
+        60 yasindan buyukse "Emekli olabilir" yazdirin
+
+        Eger calisan erkek ise;
+        65 yasindan buyukse "Emekli olabilir" yazdirin
+
+
+         */
+    }
+}
