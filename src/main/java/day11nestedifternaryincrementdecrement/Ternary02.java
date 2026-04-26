8f13848d8b5ea58132731a88efd6dec37a003e9f
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-04-26 10:43:13 +0200

    v

--- src/main/java/day11nestedifternaryincrementdecrement/Ternary02.java
+++ src/main/java/day11nestedifternaryincrementdecrement/Ternary02.java
@@ -8,8 +8,15 @@ public class Ternary02 {
         int b = 20;
 
         int r1 = a<b ? a++ : ++b;
-        System.out.println(r1);
+        System.out.println(r1);//10
         System.out.println(a);//11
         System.out.println(b);//20
+
+        //example 2: verilen bir sayinin mutlak degerini hesaplayan kodu yaziniz.
+        // -4 ==> -1*-4  4==>4  0==>0
+        int c = 4;
+       int r2 = c<0? -1*c : c;
+        System.out.println(r2);//4
+
     }
 }
