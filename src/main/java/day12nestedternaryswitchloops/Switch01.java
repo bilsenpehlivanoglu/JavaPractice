2c044c59378af728d81c303d574176a2331b8cb2
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-04-26 15:15:09 +0200

    v

--- src/main/java/day12nestedternaryswitchloops/Switch01.java
+++ src/main/java/day12nestedternaryswitchloops/Switch01.java
@@ -26,5 +26,32 @@ public class Switch01 {
         }else {
             System.out.println("Enter a valid day name");
         }
+
+        //second way switch
+        switch (dayName){
+            case "Sunday":
+                System.out.println(1);
+                break;
+            case "Monday":
+                System.out.println(2);
+                break;
+            case "Tuesday":
+                System.out.println(3);
+                break;
+            case "Wednesday":
+                System.out.println(4);
+                break;
+            case "Thursday":
+                System.out.println(5);
+            case "Friday":
+                System.out.println(6);
+                break;
+            case "Saturday":
+                System.out.println(7);
+                break;
+            default:
+                System.out.println("enter a vaild dayname");
+        }
+
     }
 }
