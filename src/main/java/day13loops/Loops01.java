0d6a5e6d25d2eabff6f932e0a67d7082dbb7e4c8
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-04-27 11:21:08 +0200

    v

--- src/main/java/day13loops/Loops01.java
+++ src/main/java/day13loops/Loops01.java
@@ -3,5 +3,17 @@ package day13loops;
 public class Loops01 {
     public static void main(String[] args) {
 
+        //example 1:ekrana 5 kere hi yazidirin.
+
+        //dort tane loop vardir for-loop, while loop, do while loop, for each loop
+
+        //a)for loop
+     /*   for (baslangic degeri ; loop'un calisma sarti' ; i++){
+            calisacak kodlar
+        }
+*/
+        for (int i = 1 ; i<6 ; i++){
+            System.out.println("Hi");
+        }
     }
 }
