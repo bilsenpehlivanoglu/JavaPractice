9b5ff373b83b144075f70c1fa7ff97f62b43e472
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-04-30 14:43:53 +0200

    v

--- src/main/java/day14loops/Loops01.java
+++ src/main/java/day14loops/Loops01.java
@@ -22,5 +22,21 @@ public class Loops01 {
         }
         System.out.println(ters2);//avaJ
 
+        //example 2:verilen bir stringin "Polindrome"
+        // olup olmadigini kontrol eden kodu yaziniz.soldan saga ve sagdan sola okunmasi ayni olan demektir.
+        //logic: stringi ters cevir sonrada ters hali ile duz halini karsilastir
+
+        String duz = "ey edip adanada pide ye";
+        String ters3 = "";
+        for (int i = duz.length()-1 ; i>=0 ; i--){
+            ters3 = ters3 + duz.substring(i,i+1);
+        }
+        if (duz.equals(ters3)){
+            System.out.println("It is a polindrome");
+        }else {
+            System.out.println("It is not Polindrome degildir");
+        }
+
+
     }
 }
