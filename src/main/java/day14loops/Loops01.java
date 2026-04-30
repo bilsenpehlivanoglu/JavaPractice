097038d247973cb25fd17684b71d0dbb4dce80f5
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-04-30 15:32:14 +0200

    v

--- src/main/java/day14loops/Loops01.java
+++ src/main/java/day14loops/Loops01.java
@@ -5,19 +5,19 @@ public class Loops01 {
 
         //example 1: verilen bir string i ters ceviren kodu yaziniz.
         //Java ==>avaJ
-        String t= "Java";
+        String t = "Java";
         String ters = "";
 
         //int i = t.length()-1 ; i>=0 ; i-- bu sekilde sondan baslar
-        for (int i = t.length()-1 ; i>=0 ; i--){
-            ters = ters + t.substring(i,i+1);
+        for (int i = t.length() - 1; i >= 0; i--) {
+            ters = ters + t.substring(i, i + 1);
         }
         System.out.println(ters);//avaJ
 
         //second way
         String u = "Java";
         String ters2 = "";
-        for (int i = u.length()-1 ; i>=0 ; i--){
+        for (int i = u.length() - 1; i >= 0; i--) {
             ters2 = ters2 + u.charAt(i);
         }
         System.out.println(ters2);//avaJ
@@ -28,12 +28,12 @@ public class Loops01 {
 
         String duz = "ey edip adanada pide ye";
         String ters3 = "";
-        for (int i = duz.length()-1 ; i>=0 ; i--){
-            ters3 = ters3 + duz.substring(i,i+1);
+        for (int i = duz.length() - 1; i >= 0; i--) {
+            ters3 = ters3 + duz.substring(i, i + 1);
         }
-        if (duz.equals(ters3)){
+        if (duz.equals(ters3)) {
             System.out.println("It is a polindrome");
-        }else {
+        } else {
             System.out.println("It is not Polindrome degildir");
         }
 
