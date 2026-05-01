fd400b91d03f29542d4b3660d9bfa10dcc53de66
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-05-01 15:53:03 +0200

    v

--- src/main/java/day14loops/NestedLoops02.java
+++ src/main/java/day14loops/NestedLoops02.java
@@ -1,5 +1,7 @@
 package day14loops;
 
+import java.util.Scanner;
+
 public class NestedLoops02 {
     public static void main(String[] args) {
 
@@ -9,5 +11,21 @@ public class NestedLoops02 {
         x x x x x
         x x x x x
          */
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of rows.");
+        int row = input.nextInt();
+
+        System.out.println("Enter the number of column.");
+        int column = input.nextInt();
+
+       for (int i = 1; i<= row ; i++){//satirlar icin distaki
+
+           for (int k = 1; k <= column; k++){//yanyana yazdiginda sutunlar icin calisir
+               System.out.print("x ");
+           }
+           System.out.println();//bu da satir yanyana yazdirildiktan sonra
+           // pointer a bir sonraki satira almak icin kullanilir.
+       }
     }
 }
