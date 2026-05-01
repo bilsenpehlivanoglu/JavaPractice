6d6c9ac5f443e7c2a9c8035d9c55d33717df8d75
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-05-01 12:56:41 +0200

    v

---
+++ src/main/java/day14loops/NestedLoops01.java
@@ -0,0 +1,25 @@
+package day14loops;
+
+public class NestedLoops01 {
+    public static void main(String[] args) {
+
+        //example: asagidaki ciktiyi veren kodu yaziniz
+        /*
+       Week:1
+        Day:1
+        Day:2
+        Day:3
+
+        ...........
+
+       Week:2
+        Day:1
+        Day:2
+        Day:3
+
+         */
+        for (int i = 1; i<5 ; i++){
+
+        }
+    }
+}
