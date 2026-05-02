3461b3f8ad33eaac152b43b9007ba166e3f5564d
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-05-02 11:00:05 +0200

    v

--- src/main/java/day15loops/NestedLoop.java
+++ src/main/java/day15loops/NestedLoop.java
@@ -1,7 +1,30 @@
 package day15loops;
 
+import java.util.Scanner;
+
 public class NestedLoop {
     public static void main(String[] args) {
+
+     /*
+     asagidaki sekli cizen kodu yaziniz
+     1
+     1 2
+     1 2 3
+     1 2 3 4
+     1 2 3 4 5
+      */
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of row");
+        int row = input.nextInt();
+
+        for (int i = 1; i<=row ; i++){
+
+            for (int k = 1; k<=i ; k++){
+                System.out.print(k+" ");
+            }
+            System.out.println();//alt satira gecmesi icin bunu yaziyoruz
+        }
 
 
     }
