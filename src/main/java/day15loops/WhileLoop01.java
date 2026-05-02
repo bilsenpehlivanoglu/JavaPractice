529f590ad5c3e9f1302213c483e0387c931360d0
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-05-02 17:07:19 +0200

    v

---
+++ src/main/java/day15loops/WhileLoop01.java
@@ -0,0 +1,20 @@
+package day15loops;
+
+public class WhileLoop01 {
+    public static void main(String[] args) {
+
+        //example: 3 den 6 ya kadar tam sayilay yazdirin
+        //with for loop
+        for (int i = 3; i<7 ; i++){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+
+        //with while loop
+        int i = 3;
+        while (i<7){
+            System.out.print(i+ " ");
+            i++;
+        }
+    }
+}
