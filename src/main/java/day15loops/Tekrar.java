ea3d69ff60c1eee42cf6c85ae518c2c26f3aa8b0
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-05-04 16:53:37 +0200

    v

---
+++ src/main/java/day15loops/Tekrar.java
@@ -0,0 +1,29 @@
+package day15loops;
+
+import java.util.Scanner;
+
+public class Tekrar {
+    public static void main(String[] args) {
+
+         /*
+     asagidaki sekli cizen kodu yaziniz
+     1
+     1 2
+     1 2 3
+     1 2 3 4
+     1 2 3 4 5
+      */
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter the number of row");
+        int row = input.nextInt();
+
+        for (int i = 1; i<=row; i++){
+
+            for (int k = 1; k<=i; k++){
+                System.out.print(k+" ");
+            }
+            System.out.println();
+        }
+    }
+}
