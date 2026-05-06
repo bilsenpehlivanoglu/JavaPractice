326f8f82df70ae728c08243f7fd2995afa259e93
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-05-06 17:41:34 +0200

    v

--- src/main/java/day15loops/Tekrar.java
+++ src/main/java/day15loops/Tekrar.java
@@ -14,7 +14,7 @@ public class Tekrar {
      1 2 3 4 5
       */
 
-        Scanner input = new Scanner(System.in);
+      /*  Scanner input = new Scanner(System.in);
         System.out.println("enter the number of row");
         int row = input.nextInt();
 
@@ -25,15 +25,51 @@ public class Tekrar {
             }
             System.out.println();
         }
-
-        //example: 3 den 6 ya kadar tam sayilay yazdirin
-        int i = 3;
+*/
+        //example: 3 den 6 ya kadar tam sayilari yazdirin
+       /* int i = 3;
         while (i<6){
             System.out.print(i+ " ");
             i++;
         }
+        System.out.println("------------------------------------------");
 
         //example 2: 23 den 12 ye kadar cift sayilari console yazdirin
+        int m = 23;
+        while (m>11){
+            if (m%2==0){
+                System.out.print(m+" ");
+            }
+            m--;
+        }
+        System.out.println();
+        //example 3: 6 dan 19 a kadar tum tek sayilari concole yazdiriniz.
+        int k = 6;
+        while (k<=19){
+            if (k%2 != 0){
+                System.out.print(k+" ");
+            }
+            k++;
+        }*/
+
+        //example 1: Kullanicinin verdigi sayi icin carpim tablosu olusturup concole yazdirin
+        /*
+        3==> 3x1=3
+             3x2=6
+             3x3=9
+             3x4=12
+             3x5=15...
+             3x10=30
+         */
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter a number");
+        int num = input.nextInt();
+
+        int i = 1;
+        while (i<=10){
+            System.out.println(num+" x "+i+" = "+(num*i) );
+            i++;
+        }
 
     }
 }
