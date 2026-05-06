9c0439f52ec0d3bec3ea649310b0cdbe49b05c8b
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-05-06 18:47:31 +0200

    v

---
+++ src/main/java/day16arraysforeachloop/Arrays01.java
@@ -0,0 +1,15 @@
+package day16arraysforeachloop;
+
+public class Arrays01 {
+    public static void main(String[] args) {
+
+        int a= 13;
+        //bu yapinin icinde sadece bir tane data depolanabilir
+        //bir yapinin icinde coklu data depolayabilmek icin Array yapisi olusturulmustur
+
+        //Array nasil olusturulur
+        String stdNames [] = new String[5];
+        System.out.println(stdNames);
+
+    }
+}
