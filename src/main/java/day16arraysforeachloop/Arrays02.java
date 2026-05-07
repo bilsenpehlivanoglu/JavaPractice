b0441d563da4c3ec8866dc5a510b60bed436fc90
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-05-07 17:29:18 +0200

    v

--- src/main/java/day16arraysforeachloop/Arrays02.java
+++ src/main/java/day16arraysforeachloop/Arrays02.java
@@ -15,5 +15,22 @@ public class Arrays02 {
         arr[4] = "Art";
         System.out.println(Arrays.toString(arr));
 
+        System.out.println(arr[0].length() + arr[arr.length - 1].length());
+
+        //example 2: string bir array olusturup icine 5 eleman ekleyin
+        //tum elemanlarin icerdigi karakter sayilari toplamini ekrana yazdirin.
+        String brr[] = new String[5];
+        brr[0] = "Miami";
+        brr[1] = "New hampshire";
+        brr[2] = "Bedford";
+        brr[3] = "New york";
+        brr[4] = "Boston";
+
+        int totalChar = 0;
+        for (int i = 0; i < brr.length; i++) {
+           totalChar = totalChar + brr[i].length();
+        }
+        System.out.println(totalChar);//39
+
     }
 }
