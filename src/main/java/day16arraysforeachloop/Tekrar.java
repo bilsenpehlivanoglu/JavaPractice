9e1c226e3c8793a901c5be237342209c8d4ea02c
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-05-08 20:11:18 +0200

    v

--- src/main/java/day16arraysforeachloop/Tekrar.java
+++ src/main/java/day16arraysforeachloop/Tekrar.java
@@ -38,5 +38,43 @@ public class Tekrar {
             System.out.println(arr[0].length() + arr[arr.length - 1].length());
 
 
+        //example 2: string bir array olusturup icine 5 eleman ekleyin
+        //tum elemanlarin icerdigi karakter sayilari toplamini ekrana yazdirin.
+        String brr[] = new String[5];
+        brr[0] = "Miami";
+        brr[1] = "New hampshire";
+        brr[2] = "Bedford";
+        brr[3] = "New york";
+        brr[4] = "Boston";
+
+        int total = 0;
+        for (int i = 0; i<brr.length; i++){
+            total = total + brr[i].length();
+        }
+        System.out.println(total);//39
+
+        //second
+        int sum = 0;
+        for (String w: brr){
+            sum = sum + w.length();
+        }
+        System.out.println(sum);//39
+
+        //example 3: notlar adinda integer bir array olusturun icine 6 tane not yerlestirin
+        // ve not ortalamasini ekrana yazdirin.
+        int notlar[] = new int[6];
+
+        notlar[0] = 60;
+        notlar[1] = 50;
+        notlar[2] = 40;
+        notlar[3] = 30;
+        notlar[4] = 20;
+        notlar[5] = 80;
+
+        int average = 0;
+        for (int w: notlar){
+            average = average + w;
+        }
+        System.out.println(average/notlar.length);//46
     }
 }
