c3c0524dc7834f30f93c24ee1c349bd197882ed0
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-05-08 14:02:27 +0200

    v

---
+++ src/main/java/day16arraysforeachloop/Arrays03.java
@@ -0,0 +1,20 @@
+package day16arraysforeachloop;
+
+import java.util.Arrays;
+
+public class Arrays03 {
+    public static void main(String[] args) {
+
+        //example 1:int  bir array olusturup icine 6 adet elaman yerlestirin.
+        // Bu elemanlarin en kucugu ile en buyugunun toplamini ekrana yazdirin
+        int ages[] = new int[6];
+        ages[0] = 20;
+        ages[1] = 23;
+        ages[2] = 19;
+        ages[3] = 44;
+        ages[4] = 15;
+        ages[5] = 33;
+
+        System.out.println(Arrays.toString(ages));//[20, 23, 19, 44, 15, 33]
+    }
+}
