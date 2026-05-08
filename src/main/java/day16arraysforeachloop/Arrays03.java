e3f8df11126b4ecf3e8f9786a02fc53f23980d66
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-05-08 19:07:07 +0200

    v

--- src/main/java/day16arraysforeachloop/Arrays03.java
+++ src/main/java/day16arraysforeachloop/Arrays03.java
@@ -33,5 +33,26 @@ public class Arrays03 {
         }
         System.out.println(maximum + minimum);//59
 
+        //example 2: String bir array olsuturun 6 eleman ekleyin. Yellow dan onceki elemanlari yazdirin
+        String colors [] = new String[6];
+
+        colors[0] = "Red";
+        colors[1] = "Orange";
+        colors[2] = "Blue";
+        colors[3] = "Yellow";
+        colors[4] = "Green";
+        colors[5] = "Brown";
+
+        System.out.println(Arrays.toString(colors));//[Red, Orange, Blue, Yellow, Green, Brown]
+
+        for (String w: colors){
+            if (w.equals("Yellow")){
+                break;
+            }
+            System.out.println(w);//Red Orange Blue
+
+
+        }
+
     }
 }
