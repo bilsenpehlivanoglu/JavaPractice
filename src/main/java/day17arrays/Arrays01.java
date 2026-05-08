7dc56d0ca54f15e038c5dcf1141ed9548a4cb2ed
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-05-08 20:56:51 +0200

    v

---
+++ src/main/java/day17arrays/Arrays01.java
@@ -0,0 +1,12 @@
+package day17arrays;
+
+import java.util.Arrays;
+
+public class Arrays01 {
+    public static void main(String[] args) {
+
+        //array leri kisa yoldan nasil olsuturabiliriz?
+        int arr[] = {63,19,313,353,7,100,4};
+        System.out.println(Arrays.toString(arr));//[63, 19, 313, 353, 7, 100, 4]
+    }
+}
