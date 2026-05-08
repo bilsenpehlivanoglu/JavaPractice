b000e99bb229ca1ba6648577d48719fba6faf5e8
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-05-08 14:30:42 +0200

    v

--- src/main/java/day16arraysforeachloop/Arrays03.java
+++ src/main/java/day16arraysforeachloop/Arrays03.java
@@ -16,5 +16,11 @@ public class Arrays03 {
         ages[5] = 33;
 
         System.out.println(Arrays.toString(ages));//[20, 23, 19, 44, 15, 33]
+        //first way
+        //sort() methodu kucukten buyuge dogri dizer
+        Arrays.sort(ages);//kucukten buyuge dogri dizildi
+        System.out.println(Arrays.toString(ages));//[15, 19, 20, 23, 33, 44]
+        System.out.println(ages[0] + ages[ages.length - 1]);//59 en kucuk en buyuk toplami
+
     }
 }
