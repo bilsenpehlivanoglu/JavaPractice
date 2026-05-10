9460e92756d0c28a2eec8efd84b0a0e23d6c8456
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-05-10 16:53:04 +0200

    v

--- src/main/java/day17arrays/Arrays02.java
+++ src/main/java/day17arrays/Arrays02.java
@@ -1,5 +1,6 @@
 package day17arrays;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Arrays02 {
@@ -13,7 +14,19 @@ public class Arrays02 {
         int numOfElements = input.nextInt();
 
         String stdNames[] = new String[numOfElements];
+        System.out.println("islemi durdurmak icin 'q' ya basiniz");
 
+        for (int i = 0; i< stdNames.length; i++){
+
+            System.out.println((i+1)+ " . ogrencinin ismini giriniz");
+            String name = input.next();
+            if (name.equalsIgnoreCase("q")){
+                break;
+            }else {
+                stdNames[i] = name;
+            }
+        }
+        System.out.println(Arrays.toString(stdNames));
 
 
     }
