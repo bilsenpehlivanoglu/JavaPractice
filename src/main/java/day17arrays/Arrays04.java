f83eb1e67564b5607d52ef37dc6f30fe534e6e0c
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-05-10 18:10:44 +0200

    v

--- src/main/java/day17arrays/Arrays04.java
+++ src/main/java/day17arrays/Arrays04.java
@@ -9,5 +9,14 @@ public class Arrays04 {
         String s = "Java is easy. Learn Java earn money";
         String words[] = s.split(" ");
         System.out.println(Arrays.toString(words));//[Java, is, easy., Learn, Java, earn, money]
+        System.out.println(words.length);//7
+
+        //example 2: size verilen bir cumlede kac harf oldugunu gosteren kodu yaziniz.
+        String letters[] = s.replaceAll("[^a-zA-Z]","").split("");
+        System.out.println(Arrays.toString(letters));
+        //[J, a, v, a, i, s, e, a, s, y, L, e, a, r, n, J, a, v, a, e, a, r, n, m, o, n, e, y]
+
+        System.out.println(letters.length);//28
+
     }
 }
