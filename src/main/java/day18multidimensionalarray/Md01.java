585d5f0faa0a5a4b72f58cf9d741e3186dd909c8
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-05-12 09:35:56 +0200

    v

--- src/main/java/day18multidimensionalarray/Md01.java
+++ src/main/java/day18multidimensionalarray/Md01.java
@@ -1,5 +1,7 @@
 package day18multidimensionalarray;
 
+import java.util.Arrays;
+
 public class Md01 {
     public static void main(String[] args) {
 
@@ -7,5 +9,15 @@ public class Md01 {
         //multidimensional array nasil olusturulur?
         int a [][] = new int[3][2];
 
+       // multidimensional array e nasil eleman eklenir?
+        a[0][0] = 5;
+        a[1][1] = 45;
+        a[2][0] = 123;
+        a[0][1] = 12;
+        a[1][0] = 81;
+        a[2][1] = 0;
+
+        //multidimensional array nasil yazdirilir?
+        System.out.println(Arrays.toString(a));
     }
 }
