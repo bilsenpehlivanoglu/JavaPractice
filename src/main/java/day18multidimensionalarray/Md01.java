50b5e6c8d125a00a9c246046f8db933ae450d477
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-05-12 09:28:19 +0200

    v

---
+++ src/main/java/day18multidimensionalarray/Md01.java
@@ -0,0 +1,11 @@
+package day18multidimensionalarray;
+
+public class Md01 {
+    public static void main(String[] args) {
+
+        //bir array in elemanlari array ise bunlar multidimensional array dir.
+        //multidimensional array nasil olusturulur?
+        int a [][] = new int[3][2];
+
+    }
+}
