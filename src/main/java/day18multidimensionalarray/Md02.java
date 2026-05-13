4b6cde41035a91c90474a6a307e9bbf7869783ca
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-05-13 09:45:09 +0200

    v

--- src/main/java/day18multidimensionalarray/Md02.java
+++ src/main/java/day18multidimensionalarray/Md02.java
@@ -6,6 +6,26 @@ public class Md02 {
         String students[][] = {{"Ali","Kemal"},{"Cemal"},{"Ayhan","Beyhan","Seyhan"},{"Ceyhan","Kayahan"}};
        //yukaridaki std arrayyindeki icinde m harfi olan isimleri console yazdiriniz.
 
-        for ()
+        for (String [] w: students){
+
+            for (String k : w){
+
+                if (k.contains("m")){
+                    System.out.println(k);//Kemal  Cemal
+                }
+            }
+        }
+
+        //example 2: bir integer multidimansional array olusturun, tum elemanlarin carpimin hesaplayiniz
+        int nums [][] = {{5,4},{2,3,2},{7}};
+        int result = 1;
+
+        for (int [] w: nums){
+
+            for (int k : w){
+                result = result*k;
+            }
+        }
+        System.out.println(result);//1680
     }
 }
