4e60bfc6782c174c37c5f7696b78f67b1f94313f
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-05-13 09:15:01 +0200

    v

---
+++ src/main/java/day18multidimensionalarray/Md02.java
@@ -0,0 +1,11 @@
+package day18multidimensionalarray;
+
+public class Md02 {
+    public static void main(String[] args) {
+
+        String students[][] = {{"Ali","Kemal"},{"Cemal"},{"Ayhan","Beyhan","Seyhan"},{"Ceyhan","Kayahan"}};
+       //yukaridaki std arrayyindeki icinde m harfi olan isimleri console yazdiriniz.
+
+        for ()
+    }
+}
