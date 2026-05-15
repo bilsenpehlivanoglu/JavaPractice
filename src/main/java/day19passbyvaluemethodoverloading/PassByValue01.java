9f0cdf141017b95bdf6c2c4f003d8c5929a4238c
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-05-15 11:35:38 +0200

    v

---
+++ src/main/java/day19passbyvaluemethodoverloading/PassByValue01.java
@@ -0,0 +1,31 @@
+package day19passbyvaluemethodoverloading;
+
+public class PassByValue01 {
+    public static void main(String[] args) {
+
+        //note: pass by value variablelerin orjinal degerlerini korur
+        int shirtPrice = 100;
+
+        System.out.println(discount("student", shirtPrice));//90
+
+        System.out.println(shirtPrice);//100 bu orjinali oldugu icin fiyati 100
+    }
+
+    //discount methodunu olusturun
+    public static int discount(String type, int price){
+        switch (type){
+            case "student":
+                price = price-10;
+                break;
+            case "veteran":
+                price = price-20;
+                break;
+            case "senior":
+                price = price-5;
+                break;
+            default:
+                price=price;
+        }
+        return price;
+    }
+}
