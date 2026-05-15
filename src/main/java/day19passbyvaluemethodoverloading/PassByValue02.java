85031df8a2fc098c70b1f58333dcb798c1b50255
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-05-15 11:57:09 +0200

    v

--- src/main/java/day19passbyvaluemethodoverloading/PassByValue02.java
+++ src/main/java/day19passbyvaluemethodoverloading/PassByValue02.java
@@ -3,6 +3,12 @@ package day19passbyvaluemethodoverloading;
 public class PassByValue02 {
     public static void main(String[] args) {
 
+   String name = "Tom Hanks;";
 
     }
+    public static String addTitle(String title, String name){
+        name = title+name;
+        return name;
+    }
+
 }
