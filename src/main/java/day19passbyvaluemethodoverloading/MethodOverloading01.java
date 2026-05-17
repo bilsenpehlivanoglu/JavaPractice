60813decde61e540a1b2bcaa34bd64e98cb6e34c
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-05-17 11:05:01 +0200

    v

--- src/main/java/day19passbyvaluemethodoverloading/MethodOverloading01.java
+++ src/main/java/day19passbyvaluemethodoverloading/MethodOverloading01.java
@@ -3,6 +3,11 @@ package day19passbyvaluemethodoverloading;
 public class MethodOverloading01 {
     public static void main(String[] args) {
 
+      add(3,5);
 
+    }
+
+    public static void add(int a,int b){
+        System.out.println(a+b);//8
     }
 }
