abb457de388a357692cfd7bd50541e45f6a651a3
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-05-19 13:48:27 +0200

    v

--- src/main/java/day20statickeywordconstructors/Car.java
+++ src/main/java/day20statickeywordconstructors/Car.java
@@ -2,9 +2,27 @@ package day20statickeywordconstructors;
 
 public class Car {
 
+    /*
+    Constructor nedir?
+    Class tan object uretmemize yarayan code blocklaridir.
+
+    Class olusturdugumuzda java bize otomatik olarak bir Constructor verir.Ama bu Constructor gozle gorulmez.
+    Gorunmeyen otomatik olarak java tarafindan verilen bu Constructor lara default Constructor denir
+
+    default  Constructor ==> Car(){  }
+     */
+
+
     String make = "Honda";
     String model = "Accord";
     int year = 2023;
     boolean hybrid = true;
+
+    public Car (String make,String model,int year,boolean hybrid){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.hybrid = hybrid;
+    }
 
 }
---
+++ src/main/java/day20statickeywordconstructors/CarRunner.java
@@ -0,0 +1,13 @@
+package day20statickeywordconstructors;
+
+public class CarRunner {
+    public static void main(String[] args) {
+
+        //siz kendi Constructor inizi olusturdugunuzda java default olani siler.
+        Car c1 = new Car("BMW","M4",2023,false);
+        Car c2 = new Car("Audi","R8",2022,true);
+        Car c3 = new Car("Honda","Civic",1999,false);
+
+
+    }
+}
