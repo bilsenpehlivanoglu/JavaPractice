0116f8f9ca752ddf966546dc9f53c786d9fb94a3
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-05-19 13:23:45 +0200

    v

---
+++ src/main/java/day20statickeywordconstructors/Car.java
@@ -0,0 +1,10 @@
+package day20statickeywordconstructors;
+
+public class Car {
+
+    String make = "Honda";
+    String model = "Accord";
+    int year = 2023;
+    boolean hybrid = true;
+
+}
---
+++ src/main/java/day20statickeywordconstructors/StaticBloks01.java
@@ -0,0 +1,18 @@
+package day20statickeywordconstructors;
+
+public class StaticBloks01 {
+
+   //bir variable olusturup ona deger atamazsak o variable yi intitialize etmemis oluruz
+    static double pi;
+
+    static { // bu bir static block tur
+        pi=3.14;
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
