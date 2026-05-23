7ed4fd7d41224c1a1f96b60c7e68539617251cca
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-05-23 10:35:27 +0200

    v

--- src/main/java/day20statickeywordconstructors/TeacherRunner.java
+++ src/main/java/day20statickeywordconstructors/TeacherRunner.java
@@ -5,5 +5,8 @@ public class TeacherRunner {
 
         Teacher t1 = new Teacher("Ali Can",40,"Istanbul");
         Teacher t2 = new Teacher("Veli Han",41);
+
+        System.out.println(t1);//Teacher{name='Ali Can', age=40, address='Istanbul'}
+        System.out.println(t2);//Teacher{name='Veli Han', age=41, address='null'}
     }
 }
