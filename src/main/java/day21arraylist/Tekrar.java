69f2c8d845a0bfa5a82480caf9abea8c66997c76
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-05-26 13:50:42 +0200

    v

--- src/main/java/day21arraylist/Tekrar.java
+++ src/main/java/day21arraylist/Tekrar.java
@@ -1,7 +1,21 @@
 package day21arraylist;
 
+import java.util.ArrayList;
+
 public class Tekrar {
     public static void main(String[] args) {
+
+        //arraylist nasil oluturulur?
+        ArrayList<Integer> ages = new ArrayList<>();
+        ages.add(8);
+        ages.add(10);
+        ages.add(15);
+        ages.add(17);
+        System.out.println(ages);//[8, 10, 15, 17]
+
+        //list e coklu eleman nasil eklenir?veya baska bir list nasil eklenir?
+
+
 
 
     }
