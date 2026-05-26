8a939d41c9510fee3180e49efa1935341062126f
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-05-26 13:44:55 +0200

    v

--- src/main/java/day22arraylistdatetime/DateTime01.java
+++ src/main/java/day22arraylistdatetime/DateTime01.java
@@ -1,7 +1,25 @@
 package day22arraylistdatetime;
 
+import java.time.LocalDate;
+
 public class DateTime01 {
     public static void main(String[] args) {
+
+        //icinde bulundugumuz zaman dilimindeki tarihi nasil aliriz?
+        LocalDate myCurrentDate = LocalDate.now();
+        System.out.println("myCurrentDate = " + myCurrentDate);//2026-05-25
+
+        //tarih ten istedigimiz bileseni nasil aliriz?
+        int monthValue = myCurrentDate.getMonthValue();
+        System.out.println(monthValue);//5 (5.ay)
+
+        int yearValue = myCurrentDate.getYear();
+        System.out.println(yearValue);//2026
+
+        int dayValue = myCurrentDate.getDayOfMonth();
+        System.out.println(dayValue);//25
+
+
 
 
     }
