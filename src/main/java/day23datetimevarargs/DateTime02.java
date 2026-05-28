c6da81cd53572f463d60496fce37701b818f44bf
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-05-28 13:44:47 +0200

    v

--- src/main/java/day23datetimevarargs/DateTime02.java
+++ src/main/java/day23datetimevarargs/DateTime02.java
@@ -1,7 +1,27 @@
 package day23datetimevarargs;
 
+import java.time.LocalTime;
+
 public class DateTime02 {
     public static void main(String[] args) {
+
+        //Anlik zamani nasil aliriz?
+        LocalTime myCurrentTime = LocalTime.now();
+        System.out.println(myCurrentTime);
+
+        //anlik zamanda bilesenler nasil alinir?
+        int hour = myCurrentTime.getHour();
+        System.out.println(hour);//13
+
+        int minute = myCurrentTime.getMinute();
+        System.out.println(minute);//43
+
+        int second = myCurrentTime.getSecond();
+        System.out.println(second);//36
+
+        int nano = myCurrentTime.getNano();
+        System.out.println(nano);//378526000
+
 
     }
 }
