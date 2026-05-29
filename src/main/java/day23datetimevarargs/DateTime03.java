90b24e53f9323fcd72fc3381642b86b948aeae92
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-05-29 14:25:25 +0200

    v

--- src/main/java/day23datetimevarargs/DateTime03.java
+++ src/main/java/day23datetimevarargs/DateTime03.java
@@ -1,12 +1,17 @@
 package day23datetimevarargs;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class DateTime03 {
     public static void main(String[] args) {
 
         LocalDateTime ldt = LocalDateTime.now();
         System.out.println(ldt);//2026-05-29T14:15:35.874008
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/dd/yyyy hh:mm");
+        String formattedLtd = dtf.format(ldt);
+        System.out.println(formattedLtd);//05/29/2026 02:21
 
     }
 }
