37a3a6260aad5f0fe76ae22e0f93f733ca178d43
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-06-02 14:53:44 +0200

    v

--- src/main/java/day24stringbuilder/Student.java
+++ src/main/java/day24stringbuilder/Student.java
@@ -1,4 +1,34 @@
 package day24stringbuilder;
 
 public class Student {
+
+    /*
+    Access modifier:
+    1)Public
+    2)Prorected
+    3)Default(Access modifier i default yapmak icin access modifier yazmayiz)
+    4)Private
+
+    Note:Access modifierlari genisden dara dogri siralayiniz
+    public > protected > default > private
+
+    public:her class dan kullanilabilir
+    protected:baska package dan kullanilamaz ama baska package de child class icinden kullanilir
+    default:baska package den kullanilamazlar
+    private:sadece olurturulduklari class icinden kullanilabilirler
+
+    Note : protected ile default arasindaki fark nedir?
+           protected olanlar baska package den kullanilamaz ancak basla package de child class icinden kullanilabilir
+           default olanlar baska package den kullanilamaz
+
+     */
+
+    //public her class dan kullanilabilir.
+    public String stdName = "Ali Can";
+
+    //protected baska packageden sadece child class dan icinde bulundugu class tan gorulur.
+    protected String address = "Istanbul";
+
+
+
 }
