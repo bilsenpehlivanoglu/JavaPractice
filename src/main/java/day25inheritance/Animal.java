50fabd0dbf440a1324d709d0c9b5144f5eb7bf58
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-06-04 13:07:17 +0200

    v

--- src/main/java/day25inheritance/Animal.java
+++ src/main/java/day25inheritance/Animal.java
@@ -1,12 +1,26 @@
 package day25inheritance;
 
+
 public class Animal {
 
     //Inheritance:miras alma
     /*
       ozellikleri bir classstan diger classlara alan bir kavramdir.
 
+      Inheritance in faydalari:
+      1)code tekrarlarindan kurtuluruz
+      2)code tamiri (maintenance) kolay olur
+      3)child class lari daha atomik yapmis oluruz
+
+      ==>bir class i baska bir class in child classi yapmak icin extends keyword kullanilir
+      ilk yazilan child, ikinci yazilan parent olur, parent a super class da denir
+
+      ==>child class objeleri parant class tan method ve variable kullanabilirler
+      ==>parent class objectleri child class dan method ve variable kullanamazlar
+
      */
+
+
 
 
 }
