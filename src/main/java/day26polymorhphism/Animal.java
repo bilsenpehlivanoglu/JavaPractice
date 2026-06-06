be8928e2e1005106cf3f5bfc53ebfaa8fef44fb4
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-06-06 11:29:21 +0200

    v

--- src/main/java/day26polymorhphism/Animal.java
+++ src/main/java/day26polymorhphism/Animal.java
@@ -5,10 +5,10 @@ public class Animal {
     public void eat(){
         System.out.println("Animals eat...");
     }
-
     public void drink(){
         System.out.println("Animals drink...");
     }
+
 
 
 }
---
+++ src/main/java/day26polymorhphism/Cat.java
@@ -0,0 +1,12 @@
+package day26polymorhphism;
+
+public class Cat extends Animal{
+
+    public void meow(){
+        System.out.println("Cats meow...");
+    }
+
+    public void eat(){
+        System.out.println("Cats eat...");
+    }
+}
