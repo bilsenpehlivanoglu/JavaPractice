734bf5cfda76184df8b77d6e5ff37a7863c3c234
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-06-08 10:07:24 +0200

    v

--- src/main/java/day26polymorhphism/StudentRunner.java
+++ src/main/java/day26polymorhphism/StudentRunner.java
@@ -8,8 +8,16 @@ public class StudentRunner {
         System.out.println(s1.getNotOrt());
         System.out.println(s1.isSuccessful());
 
+
+        System.out.println("=============================");
+
         s1.setNotOrt(4.99);//burada degri degistirdik
         System.out.println(s1.getNotOrt());//4.99 burada okuduk
 
+        s1.setStdId("MU1234567");//MU1234567
+        System.out.println(s1.getStdId());
+
+        s1.setSuccessful(true);
+        System.out.println(s1.isSuccessful());//true
     }
 }
