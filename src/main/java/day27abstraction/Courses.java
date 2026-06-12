3df338f93e727c7ac04d18aa4cf59266160c0786
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-06-12 09:19:58 +0200

    v

--- src/main/java/day27abstraction/Courses.java
+++ src/main/java/day27abstraction/Courses.java
@@ -10,6 +10,8 @@ public abstract class Courses {
         System.out.println("Learn art");
     }
 
+
+
     /*
     eger bir classdan object olsuturulmaisni istemiyorsak ve bu claasi sadece child classlarin yapmak zorunda olduklari
      icin kullaniyorsak o zmn bu class abstract olur
