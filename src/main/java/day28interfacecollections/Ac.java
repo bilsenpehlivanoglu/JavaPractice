7fec75700b0c5eb22e8237ee4f7ef194203a0e5e
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-06-15 13:49:34 +0200

    v

--- src/main/java/day28interfacecollections/Ac.java
+++ src/main/java/day28interfacecollections/Ac.java
@@ -6,6 +6,8 @@ public interface Ac {
 
     void run();
 
+    public static final int price = 2000;
+
     /*
     1) bir interface i bir classin parenti yapmak icin implement keywordunu kullaniriz
     2)
--- src/main/java/day28interfacecollections/HondaRunner.java
+++ src/main/java/day28interfacecollections/HondaRunner.java
@@ -5,10 +5,18 @@ public class HondaRunner {
 
         Civic myCivic = new Civic();
         myCivic.cool();//Civic cool perf.
+        myCivic.eco();//Uses gas less
 
         Accord myAccord = new Accord();
         myAccord.cool();//Accord cool
         myAccord.run();//Accord runs super
+
+        System.out.println("Ac Price " +Ac.price);//Ac Price 2000
+        System.out.println("Engine price "+Engine.price);//Engine price 3000
+
+        Engine.stop();//stops securely
+
+
 
     }
 }
