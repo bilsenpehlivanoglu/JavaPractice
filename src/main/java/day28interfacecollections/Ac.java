d498cadbfa2d66ee0086afcdc7537ae677714455
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-06-15 14:06:48 +0200

    v

--- src/main/java/day28interfacecollections/Ac.java
+++ src/main/java/day28interfacecollections/Ac.java
@@ -12,4 +12,16 @@ public interface Ac {
     1) bir interface i bir classin parenti yapmak icin implement keywordunu kullaniriz
     2)
      */
+
+    /*
+    INTERVIEW SORUSU
+    Abstract class ile interface arasindaki farklar?
+    1)Abstract class lar hem Abstract hemde concrete methodlar icerebilir fakat interface ler sadece Abstract icerir
+    2)Abstract classlar multiple inheritance i desteklemez, ama interface ler destekler
+    3)Abstract classlar icinde her turlu variable olusturulabilir,
+    interface ler icindeki variable ler public,static ve final olmak zorundadir
+    4)interface class in childi olamaz, ama Abstract class in child i olabilir.
+    5)Abstract classlarda constructor vardir ama object uretemez,
+    interface lerde constructor yoktur bu yuzden object uretilemez
+     */
 }
