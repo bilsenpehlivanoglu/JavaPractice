ecf5b0aef8e56d807f452dee5fb2510271d87747
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-06-15 15:58:04 +0200

    v

---
+++ src/main/java/day28interfacecollections/LinkedList01.java
@@ -0,0 +1,14 @@
+package day28interfacecollections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LinkedList01 {
+    public static void main(String[] args) {
+
+//        ArrayList<String> list1 = new ArrayList<>();
+//        List<String> list2 = new ArrayList<>();
+
+
+    }
+}
