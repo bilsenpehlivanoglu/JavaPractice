8fdb5a1cc9d3e742ac1f26bc4413dd7a577980e3
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-06-16 11:35:33 +0200

    v

--- src/main/java/day28interfacecollections/LinkedList01.java
+++ src/main/java/day28interfacecollections/LinkedList01.java
@@ -1,6 +1,7 @@
 package day28interfacecollections;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class LinkedList01 {
@@ -9,6 +10,19 @@ public class LinkedList01 {
 //        ArrayList<String> list1 = new ArrayList<>();
 //        List<String> list2 = new ArrayList<>();
 
+        /*
+        1)ArrayList eleman
+        eleman ekleme ve silme cogunlukla yapilacaksa
+         */
+
+        LinkedList<String> s = new LinkedList<>();
+        s.add("Nuriye");
+        s.add("Orhan");
+        s.add("Bunyamin");
+        s.add("Saadet");
+        s.add("Niyazi");
+        s.add("Bilal");
+        s.add("Abdullah");
 
     }
 }
