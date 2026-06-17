f9aa3bd98ac2f4e3c66b42614af08c2623804c37
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-06-17 10:29:41 +0200

    v

--- src/main/java/day29collections/Sets01.java
+++ src/main/java/day29collections/Sets01.java
@@ -1,6 +1,8 @@
 package day29collections;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
 
 public class Sets01 {
     public static void main(String[] args) {
@@ -23,7 +25,44 @@ public class Sets01 {
         hs.add("Omer");
         hs.add("Hasan");
         hs.add("Benna");
-        System.out.println(hs);
+        System.out.println(hs);//[Hasan, Seda, Omer, Gulce, Benna]
+
+        hs.add("Seda");
+        hs.add("Null");
+        hs.add("Null");
+        System.out.println(hs);//[Null, Hasan, Seda, Omer, Gulce, Benna]
+
+        LinkedHashSet<Integer> lhs = new LinkedHashSet<>();
+        lhs.add(19);
+        lhs.add(13);
+        lhs.add(-33);
+        lhs.add(312);
+        System.out.println(lhs);//[19, 13, -33, 312] verdigimiz sirayla
+        lhs.add(null);
+        lhs.add(null);
+        System.out.println(lhs);//[19, 13, -33, 312, null]
+
+        LinkedHashSet<Integer> ls = new LinkedHashSet<>();
+        ls.add(19);
+        ls.add(13);
+        ls.add(313);
+        ls.add(45);
+        lhs.retainAll(ls);
+        System.out.println(lhs);//[19, 13]
+        System.out.println(ls);//[19, 13, 313, 45]
+
+        TreeSet<Character> ts = new TreeSet<>();
+        ts.add('E');
+        ts.add('G');
+        ts.add('B');
+        ts.add('A');
+        ts.add('L');
+        ts.add('Y');
+        ts.add('E');
+      // ts.add(null); null eklenemez
+        System.out.println(ts);//[A, B, E, G, L, y]
+
+
 
     }
 
