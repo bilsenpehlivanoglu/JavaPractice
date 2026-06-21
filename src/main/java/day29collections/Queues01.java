e26362cd7dc9ccfc93e17aecd0f83b5e4d2bd7ed
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-06-21 16:12:28 +0200

    v

---
+++ src/main/java/day29collections/Queues01.java
@@ -0,0 +1,28 @@
+package day29collections;
+
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Queues01 {
+    public static void main(String[] args) {
+
+        Queue<String> depo = new LinkedList<>();
+        depo.add("Milk");
+        depo.add("Meat");
+        depo.add("Egg");
+        depo.add("Cheese");
+        System.out.println(depo);//[Milk, Meat, Egg, Cheese]
+
+        depo.remove();//ilk elemani siliyor
+        System.out.println(depo);//[Meat, Egg, Cheese]
+
+        System.out.println(depo.peek());//Meat
+
+        depo.clear();//depo bosaldi
+        System.out.println(depo.poll());//null
+        System.out.println(depo.remove());//depo bos oldugu icin hata verir
+
+
+    }
+}
--- src/main/java/day29collections/Sets02.java
+++ src/main/java/day29collections/Sets02.java
@@ -1,11 +1,16 @@
 package day29collections;
 
+import java.util.HashSet;
 import java.util.TreeSet;
 
 public class Sets02 {
     public static void main(String[] args) {
 
         //example : ogrenci email adresslerini naturel ordera gore siralanmis sekilde depolayiniz.
+
+        Long t1 = System.nanoTime();
+
+
         //1 way
         TreeSet<String> emails = new TreeSet<>();
         emails.add("z@gmail.com");
@@ -18,5 +23,26 @@ public class Sets02 {
         emails.add("m@gmail.com");
         System.out.println(emails);
         //[a@gmail.com, b@gmail.com, e@gmail.com, h@gmail.com, k@gmail.com, m@gmail.com, v@gmail.com, z@gmail.com]
+
+        Long t2 = System.nanoTime();
+
+        //second way:
+        HashSet<String> emailsHs = new HashSet<>();
+        emailsHs.add("z@gmail.com");
+        emailsHs.add("b@gmail.com");
+        emailsHs.add("k@gmail.com");
+        emailsHs.add("v@gmail.com");
+        emailsHs.add("h@gmail.com");
+        emailsHs.add("e@gmail.com");
+        emailsHs.add("a@gmail.com");
+        emailsHs.add("m@gmail.com");
+
+        TreeSet<String> emailsHsTs = new TreeSet<>(emailsHs);
+        System.out.println(emailsHsTs);
+        //[a@gmail.com, b@gmail.com, e@gmail.com, h@gmail.com, k@gmail.com, m@gmail.com, v@gmail.com, z@gmail.com]
+        Long t3 = System.nanoTime();
+        System.out.println(t2-t1);//214875 treeset
+        System.out.println(t3-t2);//59209 hashset
+
     }
 }
