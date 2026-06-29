650dc8a572a1b3b700ea511670e169aac8456c49
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-06-29 12:45:04 +0200

    v

--- src/main/java/day30maps/HashMap02.java
+++ src/main/java/day30maps/HashMap02.java
@@ -22,6 +22,26 @@ public class HashMap02 {
         stdAges.replace("Hasan",35);
         System.out.println(stdAges);//{Hasan=35, Abdullah=24, Gulce=30, Benna=32}
 
+        //eski degeri kontrol ederek yeni degeri degistirir.
+        stdAges.replace("Gulce",30,35);
+
+        //putIfAbsent eger key olarak yoksa ekleme yapar, varsa ekleme yapmaz
+        stdAges.putIfAbsent("Suleyman",44);
+        System.out.println(stdAges);//{Suleyman=44, Hasan=35, Abdullah=24, Gulce=35, Benna=32} olmadigi icin ekledi
+
+        stdAges.putIfAbsent("Ali",18);
+        System.out.println(stdAges);//{Suleyman=44, Hasan=35, Abdullah=24, Gulce=35, Benna=32, Ali=18}
+
+        //get ve getordefault eger aranan eleman varsa ayni value verir, yoksa null verir
+        System.out.println(stdAges.get("Abdullah"));//24
+        System.out.println(stdAges.getOrDefault("Abdullah", 0));//24
+
+        //containsValue methodu map in icindeki value larin icerisinde olup olmadigini kontrol eder
+        System.out.println(stdAges.containsValue(63));//false
+
+        System.out.println(stdAges.containsKey("Seda"));//false, eleman olmadigi icin
+        System.out.println(stdAges.containsKey("Hasan"));//true, eleman oldugu icin
+
 
 
 
