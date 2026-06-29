f89e485191f6c4ba65df192f036bb9e1a96cb3d7
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-06-29 12:23:22 +0200

    v

--- src/main/java/day30maps/HashMap02.java
+++ src/main/java/day30maps/HashMap02.java
@@ -10,10 +10,20 @@ public class HashMap02 {
         stdAges.put("Abdullah",39);
         stdAges.put("Benna",32);
         stdAges.put("Hasan",30);
-        stdAges.put("Suleyman",33);
+
+
+        //key i tekrarli kullanmdigimizda, en son yazdigimiz verilen entry nin degerini kabul eder
         stdAges.put("Abdullah",24);
 
-        System.out.println(stdAges);//{Suleyman=33, Hasan=30, Abdullah=39, Gulce=30, Benna=32}
+        //siralama yaparken rastgele siralar bu yuzden hizlidir
+        System.out.println(stdAges);//{Suleyman=33, Hasan=30, Abdullah=24, Gulce=30, Benna=32}
+
+        //replace methodu key in value sini degistirmek icin bunu kullaniriz(deger degistirmek icin)
+        stdAges.replace("Hasan",35);
+        System.out.println(stdAges);//{Hasan=35, Abdullah=24, Gulce=30, Benna=32}
+
+
+
 
     }
 }
