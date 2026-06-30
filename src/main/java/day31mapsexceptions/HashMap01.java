b7c4661e8046c068cf701c01e959d85821ecb4d6
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-06-30 14:05:42 +0200

    v

---
+++ src/main/java/day31mapsexceptions/HashMap01.java
@@ -0,0 +1,14 @@
+package day31mapsexceptions;
+
+import java.util.HashMap;
+
+public class HashMap01 {
+    public static void main(String[] args) {
+
+        //hashmap arkasinda calisan sistem nasildir?
+        HashMap<String,String> capitals = new HashMap<>();
+        capitals.put("USA","Washington");
+
+        capitals.get("USA");
+    }
+}
