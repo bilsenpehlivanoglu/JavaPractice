a889ac1923cc050fead0b1a31f2c615464a47bb0
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-06-30 12:30:38 +0200

    v

--- src/main/java/day30maps/HashMap03.java
+++ src/main/java/day30maps/HashMap03.java
@@ -1,5 +1,8 @@
 package day30maps;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class HashMap03 {
     public static void main(String[] args) {
 
@@ -14,6 +17,24 @@ public class HashMap03 {
 
         //kelimelerin gorunum sayisini bulmak icin string i kelimelere bolduk
         String[] words = s.split(" ");
+        System.out.println(Arrays.toString(words));//[I, like, you, like, like]
+
+        //words array indeki kelimeler birer birer map te var mi yok mu diye kontrol edecegiz.
+        //words array indeki kelimeler map te yoksa map e value su 1 olarak yerlestirilecek
+        //words array indeki kelimeler map te varsa map e value su 1 olarak yerlestirilecek
+
+        HashMap<String,Integer> occ = new HashMap<>();
+
+        for (String w:words){
+            Integer numOfOccurance = occ.get(w);
+
+            if (numOfOccurance==null){
+                occ.put(w,1);
+            }else {
+                occ.replace(w,numOfOccurance+1);
+            }
+        }
+        System.out.println(occ);//{like=3, I=1, you=1}
 
 
 
