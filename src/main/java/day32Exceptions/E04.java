3a61b0c7614a233f592ddb24c650fa7776ffe94c
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-07-05 12:22:27 +0200

    v

--- src/main/java/day32Exceptions/E04.java
+++ src/main/java/day32Exceptions/E04.java
@@ -2,9 +2,10 @@ package day32Exceptions;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class E04 {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
 
         /*
         throw ile throws arasindaki fark nedir? :
@@ -12,5 +13,13 @@ public class E04 {
 
 
         FileInputStream fis = new FileInputStream("src/main/java/day32Exceptions/file.txt");
+
+        int k = 0;
+
+        while ((k=fis.read())!=-1){
+            System.out.print((char) k);//Java is easy - Nothing is easy if you not study
+            //file.txt dosyasini bu kodla okuduk
+        }
+
     }
 }
