53c9a24285d7b91b2d8740669959930ad9035302
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-07-09 18:09:51 +0200

    v

---
+++ src/main/java/day33exceptionsenumiterator/InvalidStudentGradeException.java
@@ -0,0 +1,9 @@
+package day33exceptionsenumiterator;
+
+public class InvalidStudentGradeException extends Exception{
+
+    public InvalidStudentGradeException(String message){
+        super(message);
+
+    }
+}
