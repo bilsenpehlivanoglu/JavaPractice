d225e235a9ecc2b7df84abfa6dbb46fb406a07c0
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-07-18 14:36:29 +0200

    v

---
+++ src/main/java/day33exceptionsenumiterator/Iterators01.java
@@ -0,0 +1,18 @@
+package day33exceptionsenumiterator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Iterators01 {
+    public static void main(String[] args) {
+
+        List<String> myList = new ArrayList<>();
+        myList.add("Admin");
+        myList.add("Bugra");
+        myList.add("Hasan");
+        myList.add("Benna");
+        myList.add("Ali");
+        System.out.println(myList);//[Admin, Bugra, Hasan, Benna, Ali]
+
+    }
+}
