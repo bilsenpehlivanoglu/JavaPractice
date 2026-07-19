99b52616a50d41761d6a12c3d9d496de53114cf0
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-07-19 13:57:50 +0200

    v

--- src/main/java/day34lambda/Lambda01.java
+++ src/main/java/day34lambda/Lambda01.java
@@ -1,8 +1,94 @@
 package day34lambda;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
 public class Lambda01 {
     public static void main(String[] args) {
 
+    /*
+    Lambda: Functional Programing dir, Digeri ise Structurel Programming.
+    1) Functional Programing de ne (ne yapilacak) (what to do) yapilacagi uzerine yogunlasilir
+    2) Structurel Programming de ise (nasil yapilacak) (how to do) yapilacagi uzerine yogunlasilir
+     */
+
+        List<Integer> nums = new ArrayList<Integer>();
+        nums.add(12);
+        nums.add(9);
+        nums.add(131);
+        nums.add(14);
+        nums.add(9);
+        nums.add(10);
+        nums.add(4);
+        nums.add(12);
+        nums.add(15);
+
+        printElements1(nums);
+        System.out.println();
+        printElements2(nums);
+        System.out.println();
+        printEven1(nums);
+        System.out.println();
+        printEven2(nums);
+        System.out.println();
+        printsSquareOfOddElements(nums);
+
 
     }
-}
+
+        // 1) bir listteki elemanlari ayni satirda aralarinda bosluk koyarak yaziniz Structurel
+         public static void printElements1( List<Integer> nums) {
+             for (Integer w : nums) {
+                 System.out.print(w + " ");//12 9 131 14 9 10 4 12 15
+
+             }
+
+         }
+
+            // 2) bir listteki elemanlari ayni satirda aralarinda bosluk koyarak yaziniz Functional
+             public static void printElements2( List<Integer> nums) {
+               nums.stream().forEach(t-> System.out.print(t+" "));
+
+                 }
+
+
+
+            //3) bir listteki cift elemanlari ayni satirda aralarina bosluk koyarak yazdiran methodu olusturunuz.
+               public static void printEven1(List<Integer> nums){
+
+                  for (Integer w: nums) {
+                      if (w%2==0){
+                          System.out.print(w+" ");//12 14 10 4 12
+                      }
+                  }
+               }
+
+            //4) bir listteki cift elemanlari ayni satirda aralarina bosluk koyarak yazdiran methodu olusturunuz.
+            public static void printEven2(List<Integer> nums){
+
+                 nums.stream().
+                         filter(t->t%2==0).
+                         forEach(t-> System.out.print(t+" "));
+               }
+
+
+           //5) bir listteki tek sayi olan elemanlarin karelerini ayni satirda aralarina
+           // bosluk koyarak yazdiran methodu olusturunuz.
+           public static void printsSquareOfOddElements(List<Integer> nums){
+                nums.stream().
+                        filter(t->t%2!=0).
+                        map(t->t*t).
+                        forEach(t-> System.out.print(t+" "));//81 17161 81 225
+          }
+
+    //6) bir listteki tek sayi olan elemanlarin kuplerini tekrarsiz ayni satirda aralarina
+    // bosluk koyarak yazdiran methodu olusturunuz.
+
+    public static void printCubeOfDistinctOddElements(){
+
+    }
+
+
+     }
+
