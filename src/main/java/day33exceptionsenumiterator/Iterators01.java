05db9bd412f27138b0f2535d670d965636c08915
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-07-19 10:30:11 +0200

    v

--- src/main/java/day33exceptionsenumiterator/Iterators01.java
+++ src/main/java/day33exceptionsenumiterator/Iterators01.java
@@ -1,7 +1,9 @@
 package day33exceptionsenumiterator;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 public class Iterators01 {
     public static void main(String[] args) {
@@ -13,6 +15,52 @@ public class Iterators01 {
         myList.add("Benna");
         myList.add("Ali");
         System.out.println(myList);//[Admin, Bugra, Hasan, Benna, Ali]
+
+        //iterator kullanip eleman silalim
+        Iterator<String> myIterator = myList.iterator();//[Admin, Bugra, Hasan, Benna, Ali]
+
+        while (myIterator.hasNext()){
+            myIterator.next();
+            myIterator.remove();
+        }
+        System.out.println(myList);
+
+        List<String> yourList = new ArrayList<>();
+        yourList.add("Admin");
+        yourList.add("Bugra");
+        yourList.add("Hasan");
+        System.out.println(yourList);//[Admin, Bugra, Hasan]
+
+        ListIterator<String> yourListItr = yourList.listIterator();
+
+        while (yourListItr.hasNext()){
+            String el = yourListItr.next();
+            yourListItr.set(el+"*");//set update eder
+        }
+        System.out.println(yourList);//[Admin*, Bugra*, Hasan*]
+
+        //previous method kullanimi
+        List<String> List3 = new ArrayList<>();
+        List3.add("Admin");
+        List3.add("Bugra");
+        List3.add("Hasan");
+        System.out.println(List3);
+
+        ListIterator<String> listItr = List3.listIterator();
+
+        //pointer i en saga almak icin
+        while (listItr.hasNext()){
+            listItr.next();
+        }
+
+        //elemanlari en sondan basa dogru yazdiriniz
+        while (listItr.hasPrevious()){
+            String el = listItr.previous();
+            System.out.println(el+ "<===");//Hasan<===   Bugra<===   Admin<===
+        }
+
+
+
 
     }
 }
---
+++ src/main/java/day34lambda/Lambda01.java
@@ -0,0 +1,8 @@
+package day34lambda;
+
+public class Lambda01 {
+    public static void main(String[] args) {
+
+
+    }
+}
